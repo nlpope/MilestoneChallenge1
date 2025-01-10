590e73631513bf90eedce6e86846fad1b6d6fcb6
Noah Pope <npope@tutanota.com>
2025-01-10 10:07:36 -0600

    Initial Commit

---
+++ MilestoneChallenge1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneChallenge1
+//
+//  Created by Noah Pope on 1/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MilestoneChallenge1Tests/MilestoneChallenge1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MilestoneChallenge1Tests.swift
+//  MilestoneChallenge1Tests
+//
+//  Created by Noah Pope on 1/10/25.
+//
+
+import Testing
+@testable import MilestoneChallenge1
+
+struct MilestoneChallenge1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
