904b58f3b1d5ddb5af273df4e90d4e31752aff96
Noah Pope <npope@tutanota.com>
2025-01-10 12:09:46 -0600

    replacing images with relevant country flags & populating cells successfully with working vc push on selection

--- MilestoneChallenge1/TableVC.swift
+++ MilestoneChallenge1/TableVC.swift
@@ -13,6 +13,7 @@ class TableVC: UITableViewController
 
     override func viewDidLoad()
     {
+        print("vdl reached")
         super.viewDidLoad()
         setUpNavigation()
         loadBundle()
@@ -32,7 +33,35 @@ class TableVC: UITableViewController
         let path    = Bundle.main.resourcePath!
         let items   = try! fm.contentsOfDirectory(atPath: path)
         
-        for item in items { if item.hasPrefix("nssl") { pictures.append(item)} }
+        for item in items { if item.hasSuffix(".png") { pictures.append(item)} }
+    }
+}
+
+
+// MARK: TABLEVIEW DELEGATE METHODS
+
+extension TableVC
+{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("numofRowsreached")
+        return pictures.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("cellforrow at reached")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FlagCell", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "DetailVC") as? DetailVC {
+            print("workin")
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
 
